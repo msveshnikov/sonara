7a7d2e3e45789e280090b449162576196c91a720
Max Sveshnikov <msveshnikov@thumbtack.net>
2014-12-13 18:40:06 +0600

    no message

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,5 +1,4 @@
 class WordsController < ApplicationController
-  before_action :set_word, only: [:show, :edit, :update, :destroy]
 
   # GET /words
   def index
@@ -9,51 +8,8 @@ class WordsController < ApplicationController
 
   # GET /words/1
   def show
+    @word=Word.find(params[:id])
   end
 
-  # GET /words/new
-  def new
-    @word = Word.new
-  end
 
-  # GET /words/1/edit
-  def edit
-  end
-
-  # POST /words
-  def create
-    @word = Word.new(word_params)
-
-    if @word.save
-      redirect_to @word, notice: 'Word was successfully created.'
-    else
-      render action: 'new'
-    end
-  end
-
-  # PATCH/PUT /words/1
-  def update
-    if @word.update(word_params)
-      redirect_to @word, notice: 'Word was successfully updated.'
-    else
-      render action: 'edit'
-    end
-  end
-
-  # DELETE /words/1
-  def destroy
-    @word.destroy
-    redirect_to words_url, notice: 'Word was successfully destroyed.'
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_word
-      @word = Word.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def word_params
-      params[:word]
-    end
 end
