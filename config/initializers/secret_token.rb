6984b059ab15becee25ab38f2ac129d6b942333b
Max Sveshnikov <msveshnikov@thumbtack.net>
2014-12-13 16:40:49 +0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sonara::Application.config.secret_key_base = '3d52e827b8d541dc18991806ae1dfb92761baad4a341240f4876b6acaece75631728d4d9b41e8966b6336ce7cb36c5c0d193b270b9b01074aaed0e742f7814f6'
