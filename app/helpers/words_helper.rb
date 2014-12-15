7d15f9c1f02c8011bf069865d546d646b8ae347f
Max Sveshnikov <msveshnikov@thumbtack.net>
2014-12-16 01:10:12 +0600

    Authors pictures

--- app/helpers/words_helper.rb
+++ app/helpers/words_helper.rb
@@ -1,8 +1,10 @@
 module WordsHelper
   def display_header(header, field)
     html = ''
-    html << "<h3>" +header + "</h3>" if !field.blank?
-    html << "<p>" + field +"</p>"
+    text = @word.send(field)
+    html << "<h3>" +header + "</h3>" unless text.blank?
+    html << image_tag(field+".jpg", width: 40) +"<br><br>".html_safe unless text.blank? || !File.exist?("app/assets/images/"+field+".jpg")
+    html << "<p>" + text +"</p>"
     return html.html_safe
   end
 end
