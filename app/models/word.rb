c7941dbfa221db2a94439dbe0fc3b39e839ae9ae
Max Sveshnikov <msveshnikov@thumbtack.net>
2014-12-16 17:37:30 +0600

    fixes

--- app/models/word.rb
+++ app/models/word.rb
@@ -10,6 +10,6 @@ class Word < ActiveRecord::Base
   # It returns the articles whose titles contain one or more words that form the query
   def self.search(query)
     # where(:title, query) -> This would return an exact match of the query
-    where("lower(name) like ?", "%#{query.mb_chars.downcase}%")
+    where("lower(name) like ?", "%#{query}%")
   end
 end
