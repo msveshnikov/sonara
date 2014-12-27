942c970c168da46b2ede465f6a0f8e890d4bc48c
Max Sveshnikov <msveshnikov@thumbtack.net>
2014-12-27 13:58:20 +0600

    staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
 set :stage, :staging
-#set :rails_env, :production
+set :rails_env, :staging
 server 'extender.tk', user: 'ubuntu', roles: %w{web app db}
 set :deploy_to, '/home/ubuntu/sonara-staging'
