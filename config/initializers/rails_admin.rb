d229f414f1a7982fd4651693d2d7a7a760236f60
Max Sveshnikov <msveshnikov@gmail.com>
2015-03-22 08:44:54 +0600

    admin auth

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -31,4 +31,10 @@ RailsAdmin.config do |config|
     # history_index
     # history_show
   end
+
+  config.authorize_with do
+    authenticate_or_request_with_http_basic('Site Message') do |username, password|
+      username == 'max' && password == 'j262'
+    end
+  end
 end
